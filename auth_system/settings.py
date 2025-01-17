b4f15253f3e9abb288fd94b62b2df896659bc853
marat <marat.shchur@gmail.com>
2025-01-17 16:47:46 +0300

    req update

--- auth_system/settings.py
+++ auth_system/settings.py
@@ -14,6 +14,11 @@ from pathlib import Path
 import os
 import dj_database_url 
 
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=True, cast=bool)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,11 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')# SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
-
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['auth-production-app.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -81,19 +83,20 @@ WSGI_APPLICATION = 'auth_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {     
-		'default': {
-      	'ENGINE': 'django.db.backends.postgresql',
-      	'HOST' : os.environ.get('POSTGRES_HOST', 'localhost'),
-      	'NAME': os.environ.get('POSTGRES_DB', 'auth'),
-      	'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-      	'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '1111'),
-      	'PORT': os.environ.get('POSTGRES_PORT', '5432'),
-    }
-}
-# DATABASES = {
-#     'default': dj_database_url.config(default='postgres://localhost')
+# DATABASES = {     
+# 		'default': {
+#       	'ENGINE': 'django.db.backends.postgresql',
+#       	'HOST' : os.environ.get('POSTGRES_HOST', 'localhost'),
+#       	'NAME': os.environ.get('POSTGRES_DB', 'auth'),
+#       	'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+#       	'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '1111'),
+#       	'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+#     }
 # }
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost')
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
